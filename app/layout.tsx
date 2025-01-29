f669dd4e0d63e35cadceb191a23ab1796981fde9
Christopher Schnick <crschnick@xpipe.io>
2025-01-29 05:14:02 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,15 +15,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>
-            <DocsLayout tree={source.pageTree} {...baseOptions} sidebar={{
-
-                tabs: {
-                    transform: (option, node) => ({
-                        ...option,
-                        icon: 'my icon',
-                    }),
-                },
-            }}>
+            <DocsLayout tree={source.pageTree} {...baseOptions}>
                 {children}
             </DocsLayout>
         </RootProvider>
