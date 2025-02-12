fb3982728ba47b1dcc152de5f0777eb3ef62a819
Christopher Schnick <crschnick@xpipe.io>
2025-02-12 17:53:06 +0100

    Rework

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -8,6 +8,7 @@ import {
 import { notFound } from 'next/navigation';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import { Callout } from 'fumadocs-ui/components/callout';
+import { InlineTOC } from 'fumadocs-ui/components/inline-toc';
 
 export default async function Page(props: {
   params: Promise<{ slug?: string[] }>;
@@ -23,7 +24,7 @@ export default async function Page(props: {
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
-        <MDX components={{ ...defaultMdxComponents, Callout }} />
+        <MDX components={{ ...defaultMdxComponents, Callout, InlineTOC }} />
       </DocsBody>
     </DocsPage>
   );
