07f01e2eee8234b4151433efa50a70ed45c5e6d5
Christopher Schnick <crschnick@xpipe.io>
2026-01-04 10:51:43 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,23 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: "/:path",
+                headers: [
+                    {key: "Access-Control-Allow-Credentials", value: "false"},
+                    {key: "Access-Control-Allow-Origin", value: "*"},
+                    {key: "Access-Control-Allow-Methods", value: "*"},
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: ""
+                    },
+                ]
+            }
+        ]
+    }
 };
 
 export default withMDX(config);
