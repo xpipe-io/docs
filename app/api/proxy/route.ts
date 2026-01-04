8a771244f96b5643fd28f8c60637eb129c22f49f
Christopher Schnick <crschnick@xpipe.io>
2026-01-04 10:34:15 +0100

    Update route.ts

--- app/api/proxy/route.ts
+++ app/api/proxy/route.ts
@@ -1,5 +1,5 @@
 import { openapi } from '@/lib/source';
 
 export const { GET, HEAD, PUT, POST, PATCH, DELETE } = openapi.createProxy({
-    allowedOrigins: ['http://localhost'],
+    allowedOrigins: ['http://localhost:21721', 'http://localhost:21722'],
 });
