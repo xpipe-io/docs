35a3492d2765293e848a892e7981fc5c30b2663a
Christopher Schnick <crschnick@xpipe.io>
2026-01-20 04:01:29 +0100

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-row min-h-screen justify-center items-center">
+      <h1 className="text-xl text-gray-900! dark:text-gray-100!">404 | This page could not be found.</h1>
+    </div>
+  );
+};
+
+export default NotFound;
